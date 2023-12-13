c164d28e5bce0afda89c3933cf362270f3c3a0b1
Todd Suess <todd@datacomponents.net>
2023-12-12 20:35:22 -0500

    opaque_ptr now uses bit mask for flags

--- include/txl/opaque_ptr.h
+++ include/txl/opaque_ptr.h
@@ -8,69 +8,84 @@ namespace txl
     class opaque_ptr
     {
     private:
-        enum class storage_type
-        {
-            none,
-            unique,
-            shared,
-            raw,
-        };
+        static constexpr uint8_t UNIQUE_PTR_MASK = 0x1,
+                                 SHARED_PTR_MASK = 0x2,
+                                 SMART_PTR_MASK = UNIQUE_PTR_MASK | SHARED_PTR_MASK;
+        static constexpr uint64_t PTR_MASK = ~static_cast<uint64_t>(SMART_PTR_MASK);
         union
         {
             std::unique_ptr<Value> uptr_;
             std::shared_ptr<Value> sptr_;
         };
-        Value * ptr_ = nullptr;
-        storage_type storage_ = storage_type::none;
+        union
+        {
+            Value * ptr_ = nullptr;
+            uint64_t bits_;
+        };
+
+        Value * get_pointer()
+        {
+            return reinterpret_cast<Value *>(bits_ & PTR_MASK);
+        }
+        
+        Value const * get_pointer() const
+        {
+            return reinterpret_cast<Value const *>(bits_ & PTR_MASK);
+        }
+
+        void apply_mask(uint8_t mask)
+        {
+            // Confirm mask isn't corrupting memory
+            assert((bits_ & mask) == 0);
+            bits_ |= mask;
+        }
+
+        inline bool is_unique_ptr() const { return (bits_ & UNIQUE_PTR_MASK) != 0; }
+        inline bool is_shared_ptr() const { return (bits_ & SHARED_PTR_MASK) != 0; }
     public:
         opaque_ptr() = default;
         opaque_ptr(std::unique_ptr<Value> && p)
             : uptr_(std::move(p))
             , ptr_(uptr_.get())
-            , storage_(storage_type::unique)
         {
+            apply_mask(UNIQUE_PTR_MASK);
         }
         opaque_ptr(std::shared_ptr<Value> const & p)
             : sptr_(p)
             , ptr_(sptr_.get())
-            , storage_(storage_type::shared)
         {
+            apply_mask(SHARED_PTR_MASK);
         }
         opaque_ptr(std::shared_ptr<Value> && p)
             : sptr_(std::move(p))
             , ptr_(sptr_.get())
-            , storage_(storage_type::shared)
         {
+            apply_mask(SHARED_PTR_MASK);
         }
         opaque_ptr(Value * p)
             : ptr_(p)
-            , storage_(storage_type::raw)
         {
         }
         ~opaque_ptr()
         {
-            switch (storage_)
+            if (is_unique_ptr())
             {
-                case storage_type::unique:
-                    uptr_.~unique_ptr();
-                    break;
-                case storage_type::shared:
-                    sptr_.~shared_ptr();
-                    break;
-                case storage_type::raw:
-                case storage_type::none:
-                    break;
+                uptr_.~unique_ptr();
+            }
+            else if (is_shared_ptr())
+            {
+                sptr_.~shared_ptr();
             }
         }
         opaque_ptr(opaque_ptr const &) = delete;
         opaque_ptr(opaque_ptr &&) = default;
 
-        Value * operator->() { return ptr_; }
-        Value const * operator->() const { return ptr_; }
-        Value & operator*() { return *ptr_; }
-        Value const & operator*() const { return *ptr_; }
+        Value * operator->() { return get_pointer(); }
+        Value const * operator->() const { return get_pointer(); }
+        Value & operator*() { return *get_pointer(); }
+        Value const & operator*() const { return *get_pointer(); }
 
         operator bool() const { return has_value(); }
-        bool has_value() const { return ptr_ != nullptr; }
+        bool has_value() const { return get_pointer() != nullptr; }
     };
 }
