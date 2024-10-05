5ab50276102c9bc8d148a0bda5a81dc51653206d
Todd Suess <todd@datacomponents.net>
2024-10-04 22:20:26 -0400

    ref is ditching std::reference_wrapper<>

--- include/txl/ref.h
+++ include/txl/ref.h
@@ -1,15 +1,32 @@
 #pragma once
 
-#include <functional>
-
 namespace txl
 {
-    template<class T>
-    struct ref : std::reference_wrapper<T>
+    // This differs from std::reference_wrapper<T> because it accepts
+    // both l-values and r-values.
+    template<class Value>
+    class ref final
     {
-        auto operator*() -> T & { return this->get(); }
-        auto operator*() const -> T const & { return this->get(); }
+    private:
+        Value * value_;
+    public:
+        ref(Value & value)
+            : value_(&value)
+        {
+        }
+        ref(Value && value)
+            : value_(&value)
+        {
+            // Value should last long enough here
+        }
+
+        operator Value&() { return *value_; }
+        auto operator*() -> Value & { return *value_; }
+        auto operator*() const -> Value const & { return *value_; }
+        auto operator->() -> Value * { return value_; }
+        auto operator->() const -> Value const * { return value_; }
     };
+
 
     template<class T>
     auto make_ref(T & value) -> ref<T>
--- tests/test_ref.cpp
+++ tests/test_ref.cpp
@@ -7,6 +7,16 @@ auto set_value(txl::ref<T> value, T new_value)
     *value = new_value;
 }
 
+struct account
+{
+    int id_;
+};
+
+auto get_id(txl::ref<account> acct) -> int
+{
+    return acct->id_;
+}
+
 TXL_UNIT_TEST(ref_lifetime)
 {
     int x = 1;
@@ -16,6 +26,15 @@ TXL_UNIT_TEST(ref_lifetime)
 
     set_value(txl::make_ref(x), 3);
     assert(x == 3);
+}
+
+TXL_UNIT_TEST(ref_tests)
+{
+    auto a1 = account{1};
+    // l-value
+    assert_equal(1, get_id(a1));
+    // r-value
+    assert_equal(2, get_id(account{2}));
 }
 
 TXL_RUN_TESTS()
