1cf1d0f24558f50ef67a2dc902d5a5e4bbe79ea5
Todd Suess <todd@datacomponents.net>
2024-11-20 08:32:07 -0500

    Test timer looping

--- tests/test_event_timer.cpp
+++ tests/test_event_timer.cpp
@@ -4,7 +4,7 @@
 
 #include <chrono>
 
-TXL_UNIT_TEST(TestEventTimer)
+TXL_UNIT_TEST(TestEventTimerSteadyClock)
 {
     auto p = txl::event_poller{};
     auto t = txl::event_timer{};
@@ -21,6 +21,48 @@ TXL_UNIT_TEST(TestEventTimer)
     assert_equal(num_polled, 1);
     assert_equal(ev[0].fd(), t.fd());
 
+    p.close();
+}
+
+TXL_UNIT_TEST(TestEventTimerSystemClock)
+{
+    auto p = txl::event_poller{};
+    auto t = txl::event_timer{};
+    auto ev = txl::event_array<4>{};
+
+    auto t1 = std::chrono::system_clock::now() + std::chrono::milliseconds{50};
+
+    p.open();
+    t.open(txl::event_timer::system_clock);
+    t.set_time(t1);
+
+    p.add(t.fd(), txl::event_type::in | txl::event_type::out);
+    auto num_polled = p.poll(ev);
+    assert_equal(num_polled, 1);
+    assert_equal(ev[0].fd(), t.fd());
+
+    p.close();
+}
+
+TXL_UNIT_TEST(TestEventTimerLooping)
+{
+    auto p = txl::event_poller{};
+    auto t = txl::event_timer{};
+    auto ev = txl::event_array<4>{};
+
+    auto t1 = std::chrono::system_clock::now() + std::chrono::milliseconds{50};
+
+    p.open();
+    t.open(txl::event_timer::system_clock);
+    t.set_time(t1, std::chrono::milliseconds{50});
+
+    p.add(t.fd(), txl::event_type::in | txl::event_type::out);
+    for (auto i = 0; i < 3; ++i)
+    {
+        auto num_polled = p.poll(ev);
+        assert_equal(num_polled, 1);
+        assert_equal(ev[0].fd(), t.fd());
+    }
     p.close();
 }
 
