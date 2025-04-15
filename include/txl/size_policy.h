9811e00e0a6bcad85bf46e53ad5090e56bb71442
Todd Suess <todd@datacomponents.net>
2025-04-15 11:02:56 -0400

    size_policy working, stream_find working

--- include/txl/size_policy.h
+++ include/txl/size_policy.h
@@ -1,33 +1,41 @@
 #pragma once
 
+#include <txl/buffer_ref.h>
+#include <algorithm>
 #include <cstddef>
 
 namespace txl
 {
-    struct size_policy
+    class size_policy
     {
-        size_t value;
-
+    private:
+        size_t value_;
+    public:
         size_policy(size_t i)
-            : value(i)
+            : value_{i}
         {
         }
 
-        auto process(size_t requested, size_t actual) -> void
+        auto value() const -> size_t
         {
-            if (actual >= value)
+            return value_;
+        }
+
+        auto process(buffer_ref requested, buffer_ref actual) -> void
+        {
+            if (actual.size() >= value_)
             {
-                value = 0;
+                value_ = 0;
             }
             else
             {
-                value -= actual;
+                value_ -= actual.size();
             }
         }
 
         auto is_complete() const -> bool
         {
-            return value == 0;
+            return value_ == 0;
         }
     };
 
@@ -39,26 +47,53 @@ namespace txl
     {
         bool maybe_eof_ = false;
 
-        auto process(size_t requested, size_t actual) -> void
+        auto process(buffer_ref requested, buffer_ref actual) -> void
         {
-            maybe_eof_ = (requested > actual);
+            maybe_eof_ = (requested.size() > actual.size());
             size_policy::process(requested, actual);
         }
 
         auto is_complete() const -> bool
         {
-            return value == 0 or maybe_eof_;
+            return value() == 0 or maybe_eof_;
+        }
+    };
+    
+    class until : public size_policy
+    {
+    private:
+        buffer_ref match_;
+        size_t match_idx_ = 0;
+        bool matched_ = false;
+    public:
+        until(buffer_ref match, size_t buffer_size = 1024)
+            : size_policy(buffer_size)
+            , match_{match}
+        {
+        }
+        
+        auto is_complete() const -> bool
+        {
+            return matched_;
+        }
+
+        auto process(buffer_ref requested, buffer_ref actual) -> void
+        {
+            
         }
     };
 
     template<class... SizePolicies>
-    class one_of : public size_policy
+    class one_of final : public size_policy
     {
     private:
-        std::tuple<SizePolicies...> size_policies_;
+        using SizePolicyTuple = std::tuple<SizePolicies...>;
+        SizePolicyTuple size_policies_;
+
+        static constexpr const size_t tuple_size = std::tuple_size_v<SizePolicyTuple>;
 
         template<size_t Index>
-        auto process_policies(size_t requested, size_t actual) -> void
+        auto process_policies(buffer_ref requested, buffer_ref actual) -> void
         {
             std::get<Index>(size_policies_).process(requested, actual);
             if constexpr (Index > 0)
@@ -82,20 +117,37 @@ namespace txl
 
             return false;
         }
+
+        template<size_t Index>
+        auto policy_value() const -> size_t
+        {
+            auto val = std::get<Index>(size_policies_).value();
+            if constexpr (Index > 0)
+            {
+                return std::min(val, policy_value<Index-1>());
+            }
+            return val;
+        }
     public:
         one_of(SizePolicies && ... size_policies)
-            : size_policies_{std::forward<SizePolicies>(size_policies)...}
+            : size_policy(0) // TODO: fix this unused var
+            , size_policies_{std::forward<SizePolicies>(size_policies)...}
         {
         }
 
-        auto process(size_t requested, size_t actual) -> void
+        auto process(buffer_ref requested, buffer_ref actual) -> void
         {
-            process_policies<std::tuple_size_v<SizePolicies...>-1>(requested, actual);
+            process_policies<tuple_size-1>(requested, actual);
         }
 
         auto is_complete() const -> bool
         {
-            return is_policy_complete<std::tuple_size_v<SizePolicies...>-1>();
+            return is_policy_complete<tuple_size-1>();
+        }
+
+        auto value() const -> size_t
+        {
+            return policy_value<tuple_size-1>();
         }
     };
 }
