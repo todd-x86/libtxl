c1e7e52aab3cecefa00b60984751014433489fc9
Todd Suess <todd@datacomponents.net>
2025-05-20 23:54:51 -0400

    Adding some docs

--- include/txl/app.h
+++ include/txl/app.h
@@ -4,6 +4,12 @@
 
 namespace txl
 {
+    /**
+     * Reads the current process's executable path.
+     * Follows the "/proc/self/exe" symlink.
+     * 
+     * \return resolved path of the running executable
+     */
     inline auto get_application_path() -> std::filesystem::path
     {
         return std::filesystem::read_symlink("/proc/self/exe").parent_path();
