9f35234f20ec0c4bd505fb25adf451252377879f
Todd Suess <todd@datacomponents.net>
2025-10-30 23:34:59 -0400

    Simple btree unit tests

--- tests/test_btree.cpp
+++ tests/test_btree.cpp
@@ -7,53 +7,63 @@
 
 TXL_UNIT_TEST(simple)
 {
+    txl::btree<std::string, int> bt{3};
+    bt.insert("Ted Crilly", 1);
+    bt.insert("Dougal McGuire", 2);
+    bt.insert("Mrs. Doyle", 3);
+    bt.insert("Jack Hackett", 4);
+    bt.insert("Noel Furlong", 5);
+    bt.insert("Billy O'Dwyer", 6);
+    bt.insert("Len Brennan", 7);
+    bt.insert("Dick Byrne", 8);
+    bt.insert("Cyril MacDuff", 9);
+    bt.insert("Ted Crilly", 2);
+    bt.insert("Ted Crilly", 3);
+
+    assert_equal(*bt.find("Len Brennan"), 7);
+    assert_equal(bt.find("Tom"), nullptr);
+
+    bt.remove("Mrs. Doyle");
+    bt.remove("Ted Crilly");
+    assert_equal(bt.find("Mrs. Doyle"), nullptr);
+    assert_not_equal(bt.find("Ted Crilly"), nullptr);
+}
+
+TXL_UNIT_TEST(random)
+{
+    txl::btree<int, int> bt{3};
+    
+    std::vector<int> keys{};
+    for (auto i = 5000; i > 0; --i)
     {
-        txl::btree<std::string, int> bt{3};
-        bt.insert("Ted Crilly", 1);
-        bt.insert("Dougal McGuire", 2);
-        bt.insert("Mrs. Doyle", 3);
-        bt.insert("Jack Hackett", 4);
-        bt.insert("Noel Furlong", 5);
-        bt.insert("Billy O'Dwyer", 6);
-        bt.insert("Len Brennan", 7);
-        bt.insert("Dick Byrne", 8);
-        bt.insert("Cyril MacDuff", 9);
-        bt.insert("Ted Crilly", 2);
-        bt.insert("Ted Crilly", 3);
-
-        assert_equal(*bt.find("Len Brennan"), 7);
-        assert_equal(bt.find("Tom"), nullptr);
-
-        bt.print();
-        bt.remove("Mrs. Doyle");
-        bt.print();
-        bt.remove("Ted Crilly");
-        bt.print();
+        bt.insert(5000-i+1, i*10);
+        keys.emplace_back(i);
     }
-
-    {
-        txl::btree<int, int> bt{3};
-        
-        std::vector<int> keys{};
-        for (auto i = 5000; i > 0; --i)
-        {
-            bt.insert(5000-i+1, i*10);
-            keys.emplace_back(i);
-        }
-	std::random_device rd;
+    std::random_device rd;
     // std::mt19937 is a Mersenne Twister engine, a good general-purpose URNG
     std::mt19937 g(rd());
 
     // Shuffle the vector using std::shuffle and the random engine
-        std::shuffle(keys.begin(), keys.end(), g);
-        for (auto k : keys)
+    std::shuffle(keys.begin(), keys.end(), g);
+    for (auto k : keys)
+    {
+        if (k > 257 or k < 253)
         {
-            if (k > 257 or k < 253)
-            {
-                bt.remove(k);
-            }
+            bt.remove(k);
         }
-        bt.print();
+    }
+
+    for (auto k : keys)
+    {
+        if (k > 257 or k < 253)
+        {
+            assert_equal(bt.find(k), nullptr);
+        }
+        else
+        {
+            assert_not_equal(bt.find(k), nullptr);
+            assert_equal(*bt.find(k), (5000-k+1)*10);
+        }
     }
 }
 
