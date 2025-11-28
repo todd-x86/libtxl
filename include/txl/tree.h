ca6901f97ac4b6524925ca2d6bd5af429a8c047d
Todd Suess <todd@datacomponents.net>
2025-11-28 08:52:16 -0500

    Working on binary search tree for RB tree impl

--- include/txl/tree.h
+++ include/txl/tree.h
@@ -5,7 +5,7 @@
 
 namespace txl
 {
-    template<class Key, class Value>
+    template<class Key, class Value, template<class> class Less = std::less>
     class binary_search_tree final
     {
     private:
@@ -23,6 +23,21 @@ namespace txl
                 , children_{nullptr, nullptr}
             {
             }
+
+            auto left() const
+            {
+                return children_.first.get();
+            }
+
+            auto right() const
+            {
+                return children_.second.get();
+            }
+
+            auto is_leaf() const
+            {
+                return left() == nullptr and right() == nullptr;
+            }
         };
 
         node_ptr root_;
@@ -34,6 +49,34 @@ namespace txl
             {
                 root_ = std::make_unique<node>(std::move(data));
                 return;
+            }
+
+            auto cmp = Less<Key>{};
+
+            auto * node = root_.get();
+            while (node and not node->is_leaf())
+            {
+                auto key_lt = cmp(key, node->data_.first);
+                if (key_lt)
+                {
+                    node = node->left();
+                }
+                else
+                {
+                    node = node->right();
+                }
+            }
+            if (not node)
+            {
+                return;
+            }
+            if (cmp(key, node->data_.first))
+            {
+                node->children_.first = std::make_unique<node>(std::move(data));
+            }
+            else
+            {
+                node->children_.second = std::make_unique<node>(std::move(data));
             }
         }
     };
---
+++ tests/test_tree.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <random>
+#include <txl/unit_test.h>
+#include <txl/tree.h>
+
+#include <string>
+
+TXL_UNIT_TEST(simple)
+{
+    txl::binary_search_tree<std::string, int> bst{};
+    bst.emplace("Ted Crilly", 1);
+    bst.emplace("Dougal McGuire", 2);
+    bst.emplace("Mrs. Doyle", 3);
+    bst.emplace("Jack Hackett", 4);
+    bst.emplace("Noel Furlong", 5);
+    bst.emplace("Billy O'Dwyer", 6);
+    bst.emplace("Len Brennan", 7);
+    bst.emplace("Dick Byrne", 8);
+    bst.emplace("Cyril MacDuff", 9);
+    bst.emplace("Ted Crilly", 2);
+    bst.emplace("Ted Crilly", 3);
+
+    /*assert_equal(*bst.find("Len Brennan"), 7);
+    assert_equal(bst.find("Tom"), nullptr);
+
+    bst.remove("Mrs. Doyle");
+    bst.remove("Ted Crilly");
+    assert_equal(bst.find("Mrs. Doyle"), nullptr);
+    assert_not_equal(bst.find("Ted Crilly"), nullptr);*/
+}
+
+
+TXL_RUN_TESTS()
